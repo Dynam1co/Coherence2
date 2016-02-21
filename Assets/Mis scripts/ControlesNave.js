6c40e7fd1625963875e779f488c487398b353cb2
Antonio Jimenez Barrios <tony20_1987@hotmail.com>
2016-02-21 02:27:46 +0100

    Escena Tony

---
+++ Assets/Mis scripts/ControlesNave.js
@@ -0,0 +1,22 @@
+﻿//var speed = 0.8; var turnSpeed = 0.15;
+
+//function Start()
+//{
+//    GetComponent<Rigidbody>.useGravity = false;
+//}
+
+ 
+//function FixedUpdate() 
+//{ 
+//    if (Input.GetButton("Jump")) 
+//    { 
+//        //Spacebar by default will make it move forward 
+//        rigidbody.AddRelativeForce (Vector3.forward*speed); 
+//    } 
+//    rigidbody.AddRelativeTorque ((Input.GetAxis("Vertical"))*turnSpeed,0,0); 
+    
+//    // W key or the up arrow to turn upwards, S or the down arrow to turn downwards. 
+//    rigidbody.AddRelativeTorque (0,(Input.GetAxis("Horizontal"))*turnSpeed,0); 
+    
+//    // A or left arrow to turn left, D or right arrow to turn right. 
+//}
---
+++ Assets/ObjetosCercanos.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjetosCercanos : MonoBehaviour {
+
+    [SerializeField] float LejaniaObjetos = 2;
+    string objetos = "";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        BuscarObjetos(this.transform.position, LejaniaObjetos);
+	}
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 500, 20), "Encontrados " + objetos);
+    }
+
+    void BuscarObjetos(Vector3 center, float radius)
+    {
+        Collider[] objEncontrados = Physics.OverlapSphere(center, radius);
+        int i = 0;
+        objetos = "";
+        while (i < objEncontrados.Length)
+        {
+            Debug.Log(objEncontrados[i].name.ToString());
+            objetos = objetos + " " + objEncontrados[i].name.ToString();
+            i++;
+        }
+    }
+}
